cee1d175342be92082769c47cae91327bdbe405c
AkshathSai <akshathsai.pittala@outlook.com>
2022-07-18 14:20:19 +0530

    Enhanced Algorithm

--- FindingMaximumInUnsortedArray.c
+++ FindingMaximumInUnsortedArray.c
@@ -28,7 +28,7 @@ int main() {
     //Logic to Find the maximum number in an Unsorted Array
     int max = arr[0];
 
-    for(int i=0; i<size; i++) {
+    for(int i=1; i<size; i++) {
 
         if(arr[i]> max) {
             max = arr[i];
